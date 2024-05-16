f39a2392ecc72111ab539d15b149661daa2b0c0b
Eduxplorer <eduardo.ssilva77@senacsp.edu.br>
2024-05-16 17:22:18 -0300

    Insert e Select

--- dbLivrariaResolucao.sql
+++ dbLivrariaResolucao.sql
@@ -17,6 +17,7 @@ email varchar(100),
 primary key(idAutor)
 );
 
+
 create table tbClientes(
 idCliente int not null auto_increment,
 nome varchar(100),
@@ -33,6 +34,7 @@ idGenero int not null,
 primary key(idLivro),
 foreign key(idGenero) references tbGeneros(idGenero)
 );
+
 
 create table tbVendas(
 idVenda int not null auto_increment,
@@ -52,6 +54,7 @@ foreign key(idVenda) references tbVendas(idVenda),
 foreign key(idLivro) references tbLivros(idLivro)
 );
 
+
 create table tbEscreve(
 idLivro int not null,
 idAutor int not null,
@@ -66,3 +69,53 @@ desc tbLivros;
 desc tbVendas;
 desc tbItens_Vendas;
 desc tbEscreve;
+
+-- Cadastrando os registros nas tabelas
+
+insert into tbGeneros(descricao) values('Tecnologia');
+insert into tbGeneros(descricao) values('Ficcao');
+insert into tbGeneros(descricao) values('Historia');
+insert into tbGeneros(descricao) values('Terror');
+insert into tbGeneros(descricao) values('Comedia');
+insert into tbGeneros(descricao) values('Romance');
+insert into tbGeneros(descricao) values('Documentario');
+
+insert into tbAutores(nome, email) values('José Augusto N. G. Manzano', 'augustomanzano@hotmail.com');
+insert into tbAutores(nome, email) values('C.S. Lewis', 'clivesstaples@hotmail.com');
+insert into tbAutores(nome, email) values('Regine Pernoud', 'regine.ecclesiae@contact.com');
+insert into tbAutores(nome, email) values('Stephen King', 'edwinking@gmail.com');
+insert into tbAutores(nome, email) values('William Shakespeare', 'williamdrama@shakespeare.com');
+insert into tbAutores(nome, email) values('Matt Haig', 'hello@matthaig.com');
+insert into tbAutores(nome, email) values('Andrew leatherbarrow', 'andrewleat@contact.com');
+
+insert into tbClientes(nome, telefone) values('Gabriella Lima', '973645708');
+insert into tbClientes(nome, telefone) values('Abner Felizardo', '924693933');
+insert into tbClientes(nome, telefone) values('Alisson Samuel', '932476503');
+
+insert into tbLivros(titulo, preco, estoque, idGenero) values('Joana dArc', 144.32, 2, 3);
+insert into tbLivros(titulo, preco, estoque, idGenero) values('Algoritmos', 57.32, 10, 1);
+insert into tbLivros(titulo, preco, estoque, idGenero) values('A Anatomia de um Luto', 24.99, 5, 2);
+insert into tbLivros(titulo, preco, estoque, idGenero) values('A Comedia dos Erros', 20.30, 23, 5);
+insert into tbLivros(titulo, preco, estoque, idGenero) values('O Iluminado', 58.99, 6, 4);
+insert into tbLivros(titulo, preco, estoque, idGenero) values('Os Humanos', 41.68, 15, 6);
+insert into tbLivros(titulo, preco, estoque, idGenero) values('Chernobyl', 26.70, 30, 7);
+
+insert into tbVendas(data, total, idCliente) values('2024-05-16', 44.29, 1);
+insert into tbVendas(data, total, idCliente) values('2019-07-20', 20.30, 2);
+insert into tbVendas(data, total, idCliente) values('2020-02-22', 85.69, 3);
+
+insert into tbItens_Vendas(idVenda, idLivro, quantidade, subtotal) values(1, 3, 2, 44.29);
+insert into tbItens_Vendas(idVenda, idLivro, quantidade, subtotal) values(3, 7, 2, 85.69);
+insert into tbItens_Vendas(idVenda, idLivro, quantidade, subtotal) values(2, 4, 1, 20.30);
+
+
+insert into tbEscreve(idLivro, idAutor) values();
+
+-- Pesquisar os campos da tabela
+
+select * from tbGeneros;
+select * from tbAutores;
+select * from tbClientes;
+select * from tbLivros;
+select * from tbVendas;
+select * from tbItens_Vendas;
